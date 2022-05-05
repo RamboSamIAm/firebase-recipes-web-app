ee4fdfe74d46fe3095747cc9b5ad167932f80b71
RamboSamIAm <carterschuurmans@Icloud.com>
2022-05-05 00:56:14 -0500

    Commiting for the night

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import { useState } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
 import LoginForm from "./comps/LoginForm";
+import AddEditRecipeForm from "./comps/AddEditRecipeForm";
 
 import './App.css';
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 
 function App() {
   const [user, setUser] = useState(null)
-  console.log(process.env.REACT_APP_API_KEY)
 
   FirebaseAuthService.subscribeToAuthChanges(setUser)
 
-
+  async function handleAddRecipe(newRecipe) {
+    try {
+      //This console.log does run
+      console.log('handling is going through 1')
+      // First argument "recipes" is the name of our collection
+      const response = await FirebaseFirestoreService.createDocument('recipes', newRecipe);
+      //This console.log does not run
+      console.log('handling is going through 2')
+      alert(`Succesfully created a recipe with an ID = ${response.id}`)
+    } catch (error) {
+      alert(error.message)
+    }
+  }
 
   return (
     <div className="App">
       <div className='title-row'>
         <h1 className='title'>Firebase Recipes</h1>
         <LoginForm existingUser={user} />
+      </div>
+      <div className="main">
+        <AddEditRecipeForm handleAddRecipe={handleAddRecipe}/>
       </div>
     </div>
   );
