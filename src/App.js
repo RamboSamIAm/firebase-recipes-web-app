5bcddf5da00d4a2b1f69b643338bc05f67d97596
RamboSamIAm <carterschuurmans@Icloud.com>
2022-05-04 21:00:57 -0500

    committing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import FirebaseAuthService from "./FirebaseAuthService";
+import LoginForm from "./comps/LoginForm";
+
 import './App.css';
-// eslint-disable-next-line no-unused-vars
-import firebase from './FirebaseConfig'
+
 
 function App() {
+  const [user, setUser] = useState(null)
+  console.log(process.env.REACT_APP_API_KEY)
+
+  FirebaseAuthService.subscribeToAuthChanges(setUser)
+
+
+
   return (
     <div className="App">
       <div className='title-row'>
         <h1 className='title'>Firebase Recipes</h1>
+        <LoginForm existingUser={user} />
       </div>
     </div>
   );
--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
+    apiKey: "AIzaSyCK9J9NfVnRhDmBvqFATfLBbLE23ipF5Wg",
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_API_KEY,
+    appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 }
 
